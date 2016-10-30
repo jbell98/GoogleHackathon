4604f00ff7bf38ab1a42602fafd1fc9d5d17a6da
Tjpatton <tjpatton711@gmail.com>
2016-10-30 03:58:47 -0400

    Add files via upload

---
+++ Armor.java
@@ -0,0 +1,40 @@
+public class Armor {
+	private String name;
+	private int defense;
+	private int speed;
+	Armor (String name, int defense, int speed) {
+		this.name = name;
+		this.defense = defense;
+		this.speed = speed;
+	}
+	Armor (String name, int defense) {
+		this.name = name;
+		this.defense = defense;
+	}
+   
+   public int getDefense()
+   {
+   return defense;
+   }
+   
+   public int getSpeed()
+   {
+   return speed;
+   }
+   
+	static Armor[] armors = {
+	new Armor("Cloth", 3),
+	new Armor("Light Leather", 5),
+	new Armor("Heavy Leather", 7),
+	new Armor("Chain", 8, -1),
+	new Armor("Light Plate", 11, -2),
+	new Armor("Heavy Plate", 13, -3),
+	new Armor("Metal", 15, -4),
+	};
+   
+   // Makes Armor Accessible
+   public Armor getArmor(int armorIndex)  {
+      Armor current = armors[armorIndex];
+      return new Armor(current.name, current.defense, current.speed);
+   }   
+}
---
+++ ProtoGauntlet.java
@@ -0,0 +1,6 @@
+public class ProtoGauntlet {
+public ProtoGauntlet()
+{
+
+}
+}
